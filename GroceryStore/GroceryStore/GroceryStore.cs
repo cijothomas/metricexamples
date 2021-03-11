3ff82fe493313be0002bec7c7c4643263cf600c7
Cijo Thomas <cithomas@microsoft.com>
2021-03-11 09:05:44 -0800

    Add grocery store app with prometheus .net

---
+++ GroceryStore/GroceryStore/GroceryStore.cs
@@ -0,0 +1,46 @@
+﻿using Prometheus;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GroceryStore
+{
+    public class GroceryStore
+    {
+        private static Dictionary<string, double> priceList = new Dictionary<string, double>()
+        {
+            { "potato", 1.00 },
+            { "tomato", 3.00 },
+        };
+
+        private string storeName;
+        private static readonly Gauge ItemsSold = Metrics.CreateGauge("itemssold",
+            "Items sold",
+            "storename",
+            "customername",
+            "itemName");
+        private static readonly Summary Orders = Metrics.CreateSummary("orders",
+            "Total orders",
+            "storename",
+            "customername"
+            );
+
+        public GroceryStore(string storeName)
+        {
+            this.storeName = storeName;
+        }
+
+        public void ProcessOrder(string customerName, params (string itemName, int itemQty)[] itemsBought)
+        {
+            double totalPrice = 0;
+
+            foreach (var item in itemsBought)
+            {
+                totalPrice += item.itemQty * priceList[item.itemName];
+                ItemsSold.WithLabels(this.storeName, customerName, item.itemName).Inc(item.itemQty);
+            }
+
+            Orders.WithLabels(this.storeName, customerName).Observe(totalPrice);
+        }
+    }
+}
---
+++ GroceryStore/GroceryStore/Program.cs
@@ -0,0 +1,34 @@
+﻿using Prometheus;
+using System;
+using System.Threading;
+
+namespace GroceryStore
+{
+    class Program
+    {
+        private static readonly Counter TickTock =
+           Metrics.CreateCounter("sampleapp_ticks_total", "Just keeps on ticking");
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Metrics.SuppressDefaultMetrics();
+
+            var server = new MetricServer(hostname: "localhost", port: 1234);
+            server.Start();
+
+            var store = new GroceryStore("Seattle");
+            store.ProcessOrder("customerA", ("potato", 2), ("tomato", 3));
+            store.ProcessOrder("customerB", ("tomato", 10));
+            store.ProcessOrder("customerC", ("potato", 2));
+            store.ProcessOrder("customerA", ("tomato", 1));
+
+            while (true)
+            {
+                TickTock.Inc();
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+            }
+        }
+    }
+}
